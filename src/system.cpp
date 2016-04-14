5774e53704c9ca31d6a51d74478c765aa4b95d1f
Aluriak <lucas.bourneuf@laposte.net>
2016-04-14 17:55:22 +0200

    system: refactorize + use Astre-defined data-holder types

--- src/system.cpp
+++ src/system.cpp
@@ -1,11 +1,13 @@
 #include "system.h"
 
 
+eng::System::System() {}
 
 
-
-eng::System::System(Astre* astre, std::vector<System*> subsystems, double distance) :
-            astre(astre), subsystems(subsystems), distance(distance) {}
+eng::System::System(AstreData astre, orbit::OrbitalTrajectory trajectory,
+                    std::vector<System*> subsystems) :
+                        astre(astre), trajectory(trajectory),
+                        subsystems(subsystems) {}
 
 
 /**
@@ -16,35 +18,41 @@ eng::System::System(Astre* astre, std::vector<System*> subsystems, double distan
 std::vector<eng::System*> eng::System::from_json(std::string filename) {
     return System::from_json(utils::json_from_file(filename));
 }
-std::vector<eng::System*> eng::System::from_json(QJsonArray json) {
+std::vector<eng::System*> eng::System::from_json(QJsonArray json, double* parent_mass) {
     std::vector<eng::System*> systems;
     for(auto value : json) {
         assert(value.isObject());
         QJsonObject object = value.toObject();
         System::validate(object);
-        double distance = object[SYSTEM_JSON_KEY_DISTANCE].isNull() ?
-                          0. : object[SYSTEM_JSON_KEY_DISTANCE].toDouble();
-        // Create the new System representing object
-        systems.push_back(new System(
-            System::json_to_astre(object),
-            System::from_json(object[SYSTEM_JSON_KEY_SATELLITES].toArray()),
-            distance
-        ));
+        orbit::OrbitalTrajectory trajectory = orbit::OrbitalTrajectory(object,
+                                                                       parent_mass);
+#if DEBUG_ORBITAL_TRAJECTORY_LOGS
+        trajectory.debug();
+#endif
+        if(parent_mass != NULL and not trajectory.is_orbit()) {
+            // Logs the problem if should be orbiting something
+            std::cerr << "The System of name " << object[SYSTEM_JSON_KEY_NAME].toString().toStdString() << " doesn't have enough data to be placed in orbit. It will not be created." << std::endl;
+        } else { // no parent OR orbit found
+            // Create the new System representing object
+            AstreData astre(System::json_to_astre(object));
+            systems.push_back(new System(
+                astre,
+                trajectory,
+                System::from_json(
+                    object[SYSTEM_JSON_KEY_SATELLITES].toArray(),
+                    &std::get<0>(astre)
+                )
+            ));
+        }
     }
     return systems;
 }
 
-eng::Astre* eng::System::json_to_astre(QJsonObject json) {
-    double speed_x, speed_y = 0.;
-    speed_x = json[SYSTEM_JSON_KEY_SPEED].toArray()[0].toDouble();
-    speed_y = json[SYSTEM_JSON_KEY_SPEED].toArray()[1].toDouble();
-    double mass = json[SYSTEM_JSON_KEY_MASS].toDouble();
-    assert(json[SYSTEM_JSON_KEY_COLOR].isArray());
-    return new Astre(
-        mass, Astre::mass_to_radius(mass),
-        json[SYSTEM_JSON_KEY_DISTANCE].toDouble(), 0.,
-        unit::kilometer_to_meter(speed_x),  // km.s-1 to m.s-1
-        unit::kilometer_to_meter(speed_y),  // (Astre expects m.s-1)
+eng::AstreData eng::System::json_to_astre(QJsonObject json) {
+    return AstreData(
+        json[SYSTEM_JSON_KEY_MASS].toDouble(),
+        json[SYSTEM_JSON_KEY_RADIUS].isNull()?
+            -1. : json[SYSTEM_JSON_KEY_RADIUS].toDouble(),
         json[SYSTEM_JSON_KEY_NAME].toString().toStdString(),
         QColor(json[SYSTEM_JSON_KEY_COLOR].toArray()[0].toInt(),
                json[SYSTEM_JSON_KEY_COLOR].toArray()[1].toInt(),
@@ -60,37 +68,86 @@ eng::Astre* eng::System::json_to_astre(QJsonObject json) {
  * Data validation: validate that a JSON object is a valid system
  */
 namespace eng {inline void System_validate_logging(QJsonValue value) {
-#if DEBUG_JSON_PARSING_LOGS
         std::cerr << "System_validate_logging: QJsonValue '"
             << value.toString().toStdString() << "' profile: "
             << value.isArray() << value.isBool() << value.isDouble()
             << value.isNull() << value.isObject() << value.isString()
             << value.isUndefined() << std::endl;
-#endif
 }}
 void eng::System::validate(QJsonObject system) {
     if(not system[SYSTEM_JSON_KEY_NAME].isNull()) {
-        System_validate_logging(system[SYSTEM_JSON_KEY_NAME]);
-        assert(system[SYSTEM_JSON_KEY_NAME].isString());
+        if(not system[SYSTEM_JSON_KEY_NAME].isString()) {
+            System_validate_logging(system[SYSTEM_JSON_KEY_NAME]);
+            assert(system[SYSTEM_JSON_KEY_NAME].isString());
+        }
     }
     if(not system[SYSTEM_JSON_KEY_MASS].isNull()) {
-        System_validate_logging(system[SYSTEM_JSON_KEY_MASS]);
-        assert(system[SYSTEM_JSON_KEY_MASS].isDouble());
+        if(not system[SYSTEM_JSON_KEY_MASS].isDouble()) {
+            System_validate_logging(system[SYSTEM_JSON_KEY_MASS]);
+            assert(system[SYSTEM_JSON_KEY_MASS].isDouble());
+        }
+    }
+    if(not system[SYSTEM_JSON_KEY_RADIUS].isNull()) {
+        if(not system[SYSTEM_JSON_KEY_RADIUS].isDouble()) {
+            System_validate_logging(system[SYSTEM_JSON_KEY_RADIUS]);
+            assert(system[SYSTEM_JSON_KEY_RADIUS].isDouble());
+        }
     }
     if(not system[SYSTEM_JSON_KEY_COLOR].isNull()) {
-        System_validate_logging(system[SYSTEM_JSON_KEY_COLOR]);
-        assert(system[SYSTEM_JSON_KEY_COLOR].isArray());
+        if(not system[SYSTEM_JSON_KEY_COLOR].isArray()) {
+            System_validate_logging(system[SYSTEM_JSON_KEY_COLOR]);
+            assert(system[SYSTEM_JSON_KEY_COLOR].isArray());
+        }
     }
     if(not system[SYSTEM_JSON_KEY_SPEED].isNull()) {
-        System_validate_logging(system[SYSTEM_JSON_KEY_SPEED]);
-        assert(system[SYSTEM_JSON_KEY_SPEED].isArray());
+        if(not system[SYSTEM_JSON_KEY_SPEED].isDouble()) {
+            System_validate_logging(system[SYSTEM_JSON_KEY_SPEED]);
+            assert(system[SYSTEM_JSON_KEY_SPEED].isDouble());
+        }
     }
     if(not system[SYSTEM_JSON_KEY_SATELLITES].isNull()) {
-        System_validate_logging(system[SYSTEM_JSON_KEY_SATELLITES]);
-        assert(system[SYSTEM_JSON_KEY_SATELLITES].isArray());
+        if(not system[SYSTEM_JSON_KEY_SATELLITES].isArray()) {
+            System_validate_logging(system[SYSTEM_JSON_KEY_SATELLITES]);
+            assert(system[SYSTEM_JSON_KEY_SATELLITES].isArray());
+        }
     }
     if(not system[SYSTEM_JSON_KEY_DISTANCE].isNull()) {
-        System_validate_logging(system[SYSTEM_JSON_KEY_DISTANCE]);
-        assert(system[SYSTEM_JSON_KEY_DISTANCE].isDouble());
+        if(not system[SYSTEM_JSON_KEY_DISTANCE].isDouble()) {
+            System_validate_logging(system[SYSTEM_JSON_KEY_DISTANCE]);
+            assert(system[SYSTEM_JSON_KEY_DISTANCE].isDouble());
+        }
+    }
+    if(not system[SYSTEM_JSON_KEY_APOAPSIS].isNull()) {
+        if(not system[SYSTEM_JSON_KEY_APOAPSIS].isDouble()) {
+            System_validate_logging(system[SYSTEM_JSON_KEY_APOAPSIS]);
+            assert(system[SYSTEM_JSON_KEY_APOAPSIS].isDouble());
+        }
+    }
+    if(not system[SYSTEM_JSON_KEY_PERIAPSIS].isNull()) {
+        if(not system[SYSTEM_JSON_KEY_PERIAPSIS].isDouble()) {
+            System_validate_logging(system[SYSTEM_JSON_KEY_PERIAPSIS]);
+            assert(system[SYSTEM_JSON_KEY_PERIAPSIS].isDouble());
+        }
+    }
+    if(not system[SYSTEM_JSON_KEY_ECCENTRICITY].isNull()) {
+        if(not system[SYSTEM_JSON_KEY_ECCENTRICITY].isDouble()) {
+            System_validate_logging(system[SYSTEM_JSON_KEY_ECCENTRICITY]);
+            assert(system[SYSTEM_JSON_KEY_ECCENTRICITY].isDouble());
+        } else {
+            double eccentricity = system[SYSTEM_JSON_KEY_ECCENTRICITY].toDouble();
+            assert(0 < eccentricity and eccentricity < 1.);
+        }
+    }
+    if(not system[SYSTEM_JSON_KEY_SEMIMAJORAXIS].isNull()) {
+        if(not system[SYSTEM_JSON_KEY_SEMIMAJORAXIS].isDouble()) {
+            System_validate_logging(system[SYSTEM_JSON_KEY_SEMIMAJORAXIS]);
+            assert(system[SYSTEM_JSON_KEY_SEMIMAJORAXIS].isDouble());
+        }
+    }
+    if(not system[SYSTEM_JSON_KEY_SEMIMINORAXIS].isNull()) {
+        if(not system[SYSTEM_JSON_KEY_SEMIMINORAXIS].isDouble()) {
+            System_validate_logging(system[SYSTEM_JSON_KEY_SEMIMINORAXIS]);
+            assert(system[SYSTEM_JSON_KEY_SEMIMINORAXIS].isDouble());
+        }
     }
 }
--- src/system.h
+++ src/system.h
@@ -8,8 +8,9 @@
 #include <QJsonValue>
 #include <QStringList>
 
-#include "utils.h"
 #include "astre.h"
+#include "utils.h"
+#include "orbit.h"
 
 
 namespace eng {
@@ -20,23 +21,30 @@ namespace eng {
     class System {
 
     public:
-        System(Astre*, std::vector<System*>, double);
+        System();
+        System(AstreData, orbit::OrbitalTrajectory, std::vector<System*>);
         static std::vector<eng::System*> from_json(std::string);
-        static std::vector<eng::System*> from_json(QJsonArray);
-        static Astre* json_to_astre(QJsonObject);
+        static std::vector<eng::System*> from_json(QJsonArray, double* =NULL);
+        static AstreData json_to_astre(QJsonObject);
         static void validate(QJsonObject system);
 
+        AstreData getAstre() const { return this->astre; }
+        orbit::OrbitalTrajectory getTrajectory() { return this->trajectory; }
+        //double getMass()      const { return std::get<0>(this->astre); }
+        //double getRadius()    const { return std::get<1>(this->astre); }
+        //std::string getName() const { return std::get<2>(this->astre); }
+        //QColor getColor()     const { return std::get<3>(this->astre); }
 
         // Accessors
-        Astre* getAstre() const { return this->astre; }
         std::vector<System*> getSubsystems() const { return this->subsystems; }
-        double getDistance() const { return this->distance; }
+        std::tuple<double, double, double, double> position() const
+                        { return this->trajectory.position(); }
 
 
     private:
-        Astre* astre;
+        AstreData astre;
+        orbit::OrbitalTrajectory trajectory;
         std::vector<System*> subsystems;
-        double distance;
 
     };
 
