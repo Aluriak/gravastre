fd48a84a060516c1626412661a46b5e3c0c9515b
Aluriak <lucas.bourneuf@laposte.net>
2016-09-17 17:46:26 +0200

    system: refactorisation + include diameter attribute

--- src/system.h
+++ src/system.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <iostream>
-#include <assert.h>
+#include <cassert>
 
 #include <QJsonObject>
 #include <QJsonArray>
@@ -15,36 +15,51 @@
 
 namespace eng {
 
+
+    /**
+     * System data as tuple of three values:
+     *  AstreData, trajectory, subsystems
+     */
+    class System;
+    typedef std::tuple<AstreData, orbit::OrbitalTrajectory,
+                       std::vector<System*>> SystemData;
+
+
     /**
      * Factory of Astre, and a Composite.
      */
     class System {
 
     public:
-        System();
-        System(AstreData, orbit::OrbitalTrajectory, std::vector<System*>);
-        static std::vector<eng::System*> from_json(std::string);
-        static std::vector<eng::System*> from_json(QJsonArray, double* =NULL);
+        //System();
+        System(SystemData);
+        static std::vector<System*> from(utils::JsonConfig&);
+        static std::vector<System*> from(QJsonArray, double* =NULL);
         static AstreData json_to_astre(QJsonObject);
         static void validate(QJsonObject system);
 
+        // Accessors
         AstreData getAstre() const { return this->astre; }
         orbit::OrbitalTrajectory getTrajectory() const { return this->trajectory; }
+        double getDiameter() const { return this->diameter; }
         //double getMass()      const { return std::get<0>(this->astre); }
         //double getRadius()    const { return std::get<1>(this->astre); }
         //std::string getName() const { return std::get<2>(this->astre); }
         //QColor getColor()     const { return std::get<3>(this->astre); }
-
-        // Accessors
         std::vector<System*> getSubsystems() const { return this->subsystems; }
         std::tuple<double, double, double, double> position() const
                         { return this->getTrajectory().position(); }
 
 
     private:
+        // Methods
+        void computeDiameter();
+
+        // Attributes
         AstreData astre;
         orbit::OrbitalTrajectory trajectory;
         std::vector<System*> subsystems;
+        double diameter; // approximative maximal size in AU
 
     };
 
