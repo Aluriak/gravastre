f37231e948fa8204c9a28f2b71a69c972b1121f6
Aluriak <lucas.bourneuf@laposte.net>
2016-09-17 18:17:30 +0200

    jsonConfig: manage both new style JSON config and old style

--- src/jsonConfig.cpp
+++ src/jsonConfig.cpp
@@ -39,20 +39,30 @@ utils::JsonConfig::~JsonConfig() {
  * Parse document in order to retrieve all available data.
  */
 void utils::JsonConfig::parseDocument() {
-    assert(this->doc.isObject());
-    assert(not this->doc.isArray());
+    LOGOK
+    if(this->doc.isArray()) {  // systems are provided directly
+    LOGOK
+        // get systems
+        systems = this->doc.array();
+    } else {  // full form
+    LOGOK
+        assert(this->doc.isObject());
+        assert(not this->doc.isArray());
 
-    // get options
-    options = this->doc.object()[SYSTEM_JSON_ROOT_OPTIONS].toObject();
+        // get options
+        options = this->doc.object()[SYSTEM_JSON_ROOT_OPTIONS].toObject();
 
-    // get players
-    auto player_objects = this->doc.object()[SYSTEM_JSON_ROOT_PLAYERS].toObject().toVariantMap();
-    for(QString player_name : player_objects.keys()) {
-        this->players.push_back(new Player(std::string(player_name.toStdString())));
+        // get players
+        auto player_objects = this->doc.object()[SYSTEM_JSON_ROOT_PLAYERS].toObject().toVariantMap();
+        for(QString player_name : player_objects.keys()) {
+            this->players.push_back(new Player(std::string(player_name.toStdString())));
+        }
+
+        // get systems
+        systems = this->doc.object()[SYSTEM_JSON_ROOT_SYSTEMS].toArray();
+    LOGOK
     }
-
-    // get systems
-    systems = this->doc.object()[SYSTEM_JSON_ROOT_SYSTEMS].toArray();
+    LOGOK
 }
 
 
@@ -62,22 +72,35 @@ void utils::JsonConfig::parseDocument() {
  * Validate document.
  */
 void utils::JsonConfig::validateDocument() {
-    assert(this->doc.isObject());
-    assert(not this->doc.isArray());
-
-    // validates options
-    for(auto value : this->doc.object()[SYSTEM_JSON_ROOT_OPTIONS].toObject()) {
-        assert(value.isObject());
-    }
-
-    // validates players
-    for(auto value : this->doc.object()[SYSTEM_JSON_ROOT_PLAYERS].toObject()) {
-        assert(value.isObject());
-    }
-
-    // validates systems
-    for(auto value : this->doc.object()[SYSTEM_JSON_ROOT_SYSTEMS].toObject()) {
-        assert(value.isObject());
+    if(this->doc.isArray()) {  // systems are provided directly
+        // validates systems
+        for(auto value : this->doc.array()) {
+            assert(value.isObject());
+        }
+    } else {  // full form
+        assert(this->doc.isObject());
+        assert(not this->doc.isArray());
+        auto field_options = this->doc.object()[SYSTEM_JSON_ROOT_OPTIONS];
+        auto field_players = this->doc.object()[SYSTEM_JSON_ROOT_PLAYERS];
+        auto field_systems = this->doc.object()[SYSTEM_JSON_ROOT_SYSTEMS];
+        // validates options
+        if(not field_options.isNull()) {
+            for(auto value : field_options.toObject()) {
+                assert(value.isObject());
+            }
+        }
+        // validates players
+        if(not field_players.isNull()) {
+            for(auto value : field_players.toObject()) {
+                assert(value.isObject());
+            }
+        }
+        // validates systems
+        if(not field_systems.isNull()) {
+            for(auto value : field_systems.toObject()) {
+                assert(value.isObject());
+            }
+        }
     }
 }
 
